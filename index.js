9f5edbc9f047c2accb352a07e77e2ce55ecf671a
Jonathan Poelman <jonathan.poelman@protonmail.com>
2022-04-19 09:40:03 +0200

    Revert "changed to commonjs"
    This reverts commit 8cc62122db0421315f5098a6058018bf921b36ab.

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-const { app, errorHandler } = require("./server");
-const sparql = require("./sparql");
-const { v1: uuidV1 } = require("uuid");
+import { app, errorHandler } from "./server.js";
+import sparql from "./sparql.js";
+import { v1 as uuidV1 } from "uuid";
 
 // generates a uuid
 const uuid = uuidV1;
 
 const mu = {
-	app: app,
-	sparql: sparql,
-	SPARQL: sparql.sparql,
-	query: sparql.query,
-	update: sparql.update,
-	sparqlEscape: sparql.sparqlEscape,
-	sparqlEscapeString: sparql.sparqlEscapeString,
-	sparqlEscapeUri: sparql.sparqlEscapeUri,
-	sparqlEscapeInt: sparql.sparqlEscapeInt,
-	sparqlEscapeFloat: sparql.sparqlEscapeFloat,
-	sparqlEscapeDate: sparql.sparqlEscapeDate,
-	sparqlEscapeDateTime: sparql.sparqlEscapeDateTime,
-	sparqlEscapeBool: sparql.sparqlEscapeBool,
-	uuid,
-	errorHandler,
+  app: app,
+  sparql: sparql,
+  SPARQL: sparql.sparql,
+  query: sparql.query,
+  update: sparql.update,
+  sparqlEscape: sparql.sparqlEscape,
+  sparqlEscapeString: sparql.sparqlEscapeString,
+  sparqlEscapeUri: sparql.sparqlEscapeUri,
+  sparqlEscapeInt: sparql.sparqlEscapeInt,
+  sparqlEscapeFloat: sparql.sparqlEscapeFloat,
+  sparqlEscapeDate: sparql.sparqlEscapeDate,
+  sparqlEscapeDateTime: sparql.sparqlEscapeDateTime,
+  sparqlEscapeBool: sparql.sparqlEscapeBool,
+  uuid,
+  errorHandler,
 };
 
 const SPARQL = mu.SPARQL,
-	query = mu.query,
-	update = mu.update,
-	sparqlEscape = mu.sparqlEscape,
-	sparqlEscapeString = mu.sparqlEscapeString,
-	sparqlEscapeUri = mu.sparqlEscapeUri,
-	sparqlEscapeInt = mu.sparqlEscapeInt,
-	sparqlEscapeFloat = mu.sparqlEscapeFloat,
-	sparqlEscapeDate = mu.sparqlEscapeDate,
-	sparqlEscapeDateTime = mu.sparqlEscapeDateTime,
-	sparqlEscapeBool = mu.sparqlEscapeBool;
+  query = mu.query,
+  update = mu.update,
+  sparqlEscape = mu.sparqlEscape,
+  sparqlEscapeString = mu.sparqlEscapeString,
+  sparqlEscapeUri = mu.sparqlEscapeUri,
+  sparqlEscapeInt = mu.sparqlEscapeInt,
+  sparqlEscapeFloat = mu.sparqlEscapeFloat,
+  sparqlEscapeDate = mu.sparqlEscapeDate,
+  sparqlEscapeDateTime = mu.sparqlEscapeDateTime,
+  sparqlEscapeBool = mu.sparqlEscapeBool;
 
 export {
-	app,
-	sparql,
-	SPARQL,
-	query,
-	update,
-	sparqlEscape,
-	sparqlEscapeString,
-	sparqlEscapeUri,
-	sparqlEscapeInt,
-	sparqlEscapeFloat,
-	sparqlEscapeDate,
-	sparqlEscapeDateTime,
-	sparqlEscapeBool,
-	uuid,
-	errorHandler,
+  app,
+  sparql,
+  SPARQL,
+  query,
+  update,
+  sparqlEscape,
+  sparqlEscapeString,
+  sparqlEscapeUri,
+  sparqlEscapeInt,
+  sparqlEscapeFloat,
+  sparqlEscapeDate,
+  sparqlEscapeDateTime,
+  sparqlEscapeBool,
+  uuid,
+  errorHandler,
 };
 
 export default mu;
