274d238e342dede52eee419c11517a87e497c264
Jonathan Poelman <jonathan.poelman@protonmail.com>
2022-04-21 10:30:57 +0200

    change port 80 to 3000

--- src/server.js
+++ src/server.js
@@ -1,47 +1,48 @@
-import httpContext from 'express-http-context';
-import express from 'express';
-import bodyParser from 'body-parser';
+import httpContext from "express-http-context";
+import express from "express";
+import bodyParser from "body-parser";
 
 var app = express();
 
-var bodySizeLimit = process.env.MAX_BODY_SIZE || '100kb';
+var bodySizeLimit = process.env.MAX_BODY_SIZE || "100kb";
 
 // parse JSONAPI content type
-app.use(bodyParser.json({
-  type: function(req) { return /^application\/vnd\.api\+json/.test(req.get('content-type')); },
-  limit: bodySizeLimit
-}));
+app.use(
+	bodyParser.json({
+		type: function (req) {
+			return /^application\/vnd\.api\+json/.test(req.get("content-type"));
+		},
+		limit: bodySizeLimit,
+	})
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // set JSONAPI content type
-app.use('/', function(req, res, next) {
-  res.type('application/vnd.api+json');
-  next();
+app.use("/", function (req, res, next) {
+	res.type("application/vnd.api+json");
+	next();
 });
 
 app.use(httpContext.middleware);
 
-app.use(function(req, res, next) {
-  httpContext.set('request', req);
-  httpContext.set('response', res);
-  next();
+app.use(function (req, res, next) {
+	httpContext.set("request", req);
+	httpContext.set("response", res);
+	next();
 });
 
-const errorHandler = function(err, req, res, next) {
-  res.status(err.status || 400);
-  res.json({
-    errors: [ {title: err.message} ]
-  });
+const errorHandler = function (err, req, res, next) {
+	res.status(err.status || 400);
+	res.json({
+		errors: [{ title: err.message }],
+	});
 };
 
 // start server
-app.listen( 80, function() {
-  console.log(`Starting server on port 80 in ${app.get('env')} mode`);
+app.listen(3000, function () {
+	console.log(`Starting server on port 80 in ${app.get("env")} mode`);
 });
 
 export default app;
 
-export {
-  app,
-  errorHandler
-}
+export { app, errorHandler };
